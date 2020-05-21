24852b47736cbe4646466fad3f2ea9b1c646882c
Anais <make@makes-iMac.local>
2020-05-21 12:59:39 +0100

    Set up for users joining and disconnecting

--- server/index.js
+++ server/index.js
@@ -13,6 +13,17 @@ const io = socketio(server);
 io.on('connection', (socket) => {
     console.log('We have a new connection.');
 
+    //Pass in the event 'join' from chat.js and do something when it is emitting
+    socket.on('join', ({ name, chatRoom }, callback) => {
+        console.log(name, chatRoom);
+        //Error handling
+
+        // const error = true;
+        // if(error) {
+        //     callback({ error: 'error' });
+        // }      
+    });
+
     socket.on('disconnect', () => {
         console.log('User has just left!');
     })
