6b383a53dc62bee04d875157d37a59488a28605b
Anais <make@makes-iMac.local>
2020-05-21 17:21:04 +0100

    Generate admin messages and user messages

--- client/src/components/Chat/chat.js
+++ client/src/components/Chat/chat.js
@@ -1,36 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import queryString from 'query-string'; 
 import io from 'socket.io-client';
+import './chat.css';
 
 let socket;
 
 const Chat = ({ location }) => {
     const [ name, setName ] = useState('');
     const [ chatRoom, setChatRoom ] = useState('');
+    const [ message, setMessage ] = useState('');
+    const [ messages, setMessages ] = useState([]);
     const ENDPOINT = 'localhost:5000';
 
-    useEffect( () => {
+    useEffect(() => {
         const { name, chatRoom } = queryString.parse(location.search);
 
         socket = io(ENDPOINT);
+        
         setName(name);
-        setName(chatRoom);
+        setChatRoom(chatRoom);
 
-        socket.emit('join', { name, chatRoom }, ({ error }) => {
-           
+        socket.emit('join', { name, chatRoom }, (error) => {
+           if(error) {
+               alert(error);
+           }
         });
-
-        //Disconnect event
-        return () => {
-            socket.emit('disconnect');
-            
-            socket.off();
-        }
-
     }, [ENDPOINT, location.search]);
+
+    useEffect( () => {
+        socket.on('message', (message) => {
+            //Push new message to array of messages
+            setMessages([...messages, message]);
+        })
+        //Run useEffect only when the array of messages changes.
+    }, [messages]);
+
+    //Function for sending messages
+    const sendMessage = (event) => {
+        //Prevent reload of the whole page
+        event.preventDefault();
+        if(message) {
+            //Clears the message console
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    }
+
+    console.log(message, messages)
+
     return (
-        <h1>Chat</h1>
-    )
+        <div className="outerContainer">
+            <div className="container">
+                <input 
+                    value={message} 
+                    onChange={(event) => setMessage(event.target.value)} 
+                    onKeyPress={(event) => event.key === 'Enter' ? sendMessage(event) : null }
+                    />
+            </div>
+        </div>
+    );
 }
 
 export default Chat;
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
-const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const PORT = process.env.PORT || 5000;
 
@@ -13,22 +13,36 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection.');
 
-    //Pass in the event 'join' from chat.js and do something when it is emitting
+    //Pass in the event 'join' from chat.js and do something when it is emitted
     socket.on('join', ({ name, chatRoom }, callback) => {
-        console.log(name, chatRoom);
-        //Error handling
+        const { error, user } = addUser({ id: socket.id, name, chatRoom });
 
-        // const error = true;
-        // if(error) {
-        //     callback({ error: 'error' });
-        // }      
+        if(error) return callback(error);
+
+        socket.join(user.chatRoom);
+        //Admin generated messages
+        //Display welcome message when user joins in
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.chatRoom}`});
+        //Let other users know the new user has joined the conversation
+        socket.broadcast.to(user.chatRoom).emit('message', { user: 'admin', text: `${user.name} has joined!`});
+        socket.join(user.chatRoom);
+        
+        callback();
+    });
+
+    //User generated messages
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.chatRoom).emit('message', { user: user.name, text: message });
+        
+        callback();
     });
 
     socket.on('disconnect', () => {
         console.log('User has just left!');
-    })
+    });
 });
 
 app.use(router);
